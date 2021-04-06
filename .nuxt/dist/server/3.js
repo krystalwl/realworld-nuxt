bac659a7a5e3a1732b8047ca848fbae02832185e
krystal <18324725929@qq.com>
2021-04-06 11:31:51 +0800

    提交地址

---
+++ .nuxt/dist/server/3.js
@@ -0,0 +1,194 @@
+exports.ids = [3];
+exports.modules = {
+
+/***/ 28:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(8);
+/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(axios__WEBPACK_IMPORTED_MODULE_0__);
+
+const request = axios__WEBPACK_IMPORTED_MODULE_0___default.a.create({
+  baseURL: 'https://conduit.productionready.io/'
+});
+/* harmony default export */ __webpack_exports__["a"] = (request);
+
+/***/ }),
+
+/***/ 31:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options!./pages/login/index.vue?vue&type=template&id=3180b75e&
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"auth-page"},[_vm._ssrNode("<div class=\"container page\">","</div>",[_vm._ssrNode("<div class=\"row\">","</div>",[_vm._ssrNode("<div class=\"col-md-6 offset-md-3 col-xs-12\">","</div>",[_vm._ssrNode("<h1 class=\"text-xs-center\">"+_vm._ssrEscape(_vm._s(_vm.isLogin ? "Sign in" : "Sign up"))+"</h1> "),_vm._ssrNode("<p class=\"text-xs-center\">","</p>",[(_vm.isLogin)?_c('nuxt-link',{attrs:{"to":"/register"}},[_vm._v("Need an account?")]):_c('nuxt-link',{attrs:{"to":"/login"}},[_vm._v("Have an account?")])],1),_vm._ssrNode(" <ul class=\"error-messages\">"+(_vm._ssrList((_vm.errors),function(messages,field){return ((_vm._ssrList((messages),function(message,index){return ("<li>"+_vm._ssrEscape("\n              "+_vm._s(field)+" "+_vm._s(message)+"\n            ")+"</li>")})))}))+"</ul> <form>"+((!_vm.isLogin)?("<fieldset class=\"form-group\"><input type=\"text\" placeholder=\"Your Name\" required=\"required\""+(_vm._ssrAttr("value",(_vm.user.username)))+" class=\"form-control form-control-lg\"></fieldset>"):"<!---->")+" <fieldset class=\"form-group\"><input type=\"email\" placeholder=\"Email\" required=\"required\""+(_vm._ssrAttr("value",(_vm.user.email)))+" class=\"form-control form-control-lg\"></fieldset> <fieldset class=\"form-group\"><input type=\"password\" placeholder=\"Password\" required=\"required\""+(_vm._ssrAttr("value",(_vm.user.password)))+" class=\"form-control form-control-lg\"></fieldset> <button class=\"btn btn-lg btn-primary pull-xs-right\">"+_vm._ssrEscape("\n            "+_vm._s(_vm.isLogin ? "Sign in" : "Sign up")+"\n          ")+"</button></form>")],2)])])])}
+var staticRenderFns = []
+
+
+// CONCATENATED MODULE: ./pages/login/index.vue?vue&type=template&id=3180b75e&
+
+// EXTERNAL MODULE: ./utils/request.js
+var request = __webpack_require__(28);
+
+// CONCATENATED MODULE: ./api/user.js
+// import { request } from '@/plugins/request'
+ // 用户登录
+
+const login = data => {
+  return Object(request["a" /* default */])({
+    method: 'POST',
+    url: '/api/users/login',
+    data
+  });
+}; // 用户注册
+
+const register = data => {
+  return Object(request["a" /* default */])({
+    method: 'POST',
+    url: '/api/users',
+    data
+  });
+};
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--2-0!./node_modules/vue-loader/lib??vue-loader-options!./pages/login/index.vue?vue&type=script&lang=js&
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+const Cookie =  false ? undefined : undefined;
+/* harmony default export */ var loginvue_type_script_lang_js_ = ({
+  middleware: "notAuthenticated",
+  name: "LoginIndex",
+
+  asyncData() {},
+
+  data() {
+    return {
+      user: {
+        username: "",
+        email: "",
+        password: ""
+      },
+      errors: {} // 错误信息
+
+    };
+  },
+
+  computed: {
+    isLogin() {
+      return this.$route.path === "/login";
+    }
+
+  },
+  methods: {
+    async onSubmit() {
+      try {
+        // const { data } = await login({ user: this.user });
+        const {
+          data
+        } = this.isLogin ? await login({
+          user: this.user
+        }) : await register({
+          user: this.user
+        });
+        console.log(`data`, data); // 跳转到首页
+
+        this.$store.commit("setUser", data.user);
+        Cookie.set("user", data.user);
+        this.$router.push("/");
+      } catch (error) {
+        // console.log(`error`, error);
+        console.dir(error);
+        this.errors = error.response.data.errors;
+      }
+    }
+
+  }
+});
+// CONCATENATED MODULE: ./pages/login/index.vue?vue&type=script&lang=js&
+ /* harmony default export */ var pages_loginvue_type_script_lang_js_ = (loginvue_type_script_lang_js_); 
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__(2);
+
+// CONCATENATED MODULE: ./pages/login/index.vue
+
+
+
+function injectStyles (context) {
+  
+  
+}
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  pages_loginvue_type_script_lang_js_,
+  render,
+  staticRenderFns,
+  false,
+  injectStyles,
+  null,
+  "3b88b858"
+  
+)
+
+/* harmony default export */ var pages_login = __webpack_exports__["default"] = (component.exports);
+
+/***/ })
+
+};;
+//# sourceMappingURL=3.js.map
---
+++ .nuxt/dist/server/6.js
@@ -0,0 +1,134 @@
+exports.ids = [6];
+exports.modules = {
+
+/***/ 33:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options!./pages/profile/index.vue?vue&type=template&id=ad4b7ee2&scoped=true&
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"profile-page"},[_vm._ssrNode("<div class=\"user-info\" data-v-ad4b7ee2><div class=\"container\" data-v-ad4b7ee2><div class=\"row\" data-v-ad4b7ee2><div class=\"col-xs-12 col-md-10 offset-md-1\" data-v-ad4b7ee2><img src=\"http://i.img ur.com/Qr71crq.jpg\" class=\"user-img\" data-v-ad4b7ee2> <h4 data-v-ad4b7ee2>Eric Simons</h4> <p data-v-ad4b7ee2>\n            Cofounder @GoThinkster, lived in Aol's HQ for a few months,\n            kindalooks like Peeta from the Hunger Games\n          </p> <button class=\"btn btn-sm btn-outline-secondary action-btn\" data-v-ad4b7ee2><i class=\"ion-plus-round\" data-v-ad4b7ee2></i>  Follow Eric Simons\n          </button></div></div></div></div> <div class=\"container\" data-v-ad4b7ee2><div class=\"row\" data-v-ad4b7ee2><div class=\"col-xs-12 col-md-10 offset-md-1\" data-v-ad4b7ee2><div class=\"articles-toggle\" data-v-ad4b7ee2><ul class=\"nav nav-pills outline-active\" data-v-ad4b7ee2><li class=\"nav-item\" data-v-ad4b7ee2><a href class=\"nav-link active\" data-v-ad4b7ee2>My Articles</a></li> <li class=\"nav-item\" data-v-ad4b7ee2><a href class=\"nav-link\" data-v-ad4b7ee2>Favorited Articles</a></li></ul></div> <div class=\"article-preview\" data-v-ad4b7ee2><div class=\"article-meta\" data-v-ad4b7ee2><a href data-v-ad4b7ee2><img src=\"http://i.img ur.com/Qr71crq.jpg\" data-v-ad4b7ee2></a> <div class=\"info\" data-v-ad4b7ee2><a href class=\"author\" data-v-ad4b7ee2>Eric Simons</a><span class=\"date\" data-v-ad4b7ee2>January 20th</span></div> <button class=\"btn btn-outline-primary btn-sm pull-xs-right\" data-v-ad4b7ee2><i class=\"ion-heart\" data-v-ad4b7ee2></i> 29\n            </button></div> <a href class=\"preview-link\" data-v-ad4b7ee2><h1 data-v-ad4b7ee2>How to build webapps that scale</h1> <p data-v-ad4b7ee2>This is the description for the post.</p> <span data-v-ad4b7ee2>Read more...</span></a></div> <div class=\"article-preview\" data-v-ad4b7ee2>\n          用户设置\n          <div class=\"article-meta\" data-v-ad4b7ee2><a href data-v-ad4b7ee2><img src=\"http://i.img ur.com/N4VcUeJ.jpg\" data-v-ad4b7ee2></a> <div class=\"info\" data-v-ad4b7ee2><a href class=\"author\" data-v-ad4b7ee2>Albert Pai</a><span class=\"date\" data-v-ad4b7ee2>January 20th</span></div> <button class=\"btn btn-outline-primary btn-sm pull-xs-right\" data-v-ad4b7ee2><i class=\"ion-heart\" data-v-ad4b7ee2></i> 32\n            </button></div> <a href class=\"preview-link\" data-v-ad4b7ee2><h1 data-v-ad4b7ee2>\n              The song you won't ever stop singing. No matter how hard youtry.\n            </h1> <p data-v-ad4b7ee2>This is the description for the post.</p> <span data-v-ad4b7ee2>Read more...</span> <ul class=\"tag-list\" data-v-ad4b7ee2><li class=\"tag-default tag-pill tag-outline\" data-v-ad4b7ee2>Music</li> <li class=\"tag-default tag-pill tag-outline\" data-v-ad4b7ee2>Song</li></ul></a></div></div></div></div>")])}
+var staticRenderFns = []
+
+
+// CONCATENATED MODULE: ./pages/profile/index.vue?vue&type=template&id=ad4b7ee2&scoped=true&
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--2-0!./node_modules/vue-loader/lib??vue-loader-options!./pages/profile/index.vue?vue&type=script&lang=js&
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+/* harmony default export */ var profilevue_type_script_lang_js_ = ({
+  middleware: "authenticated",
+  name: "UserProfile"
+});
+// CONCATENATED MODULE: ./pages/profile/index.vue?vue&type=script&lang=js&
+ /* harmony default export */ var pages_profilevue_type_script_lang_js_ = (profilevue_type_script_lang_js_); 
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__(2);
+
+// CONCATENATED MODULE: ./pages/profile/index.vue
+
+
+
+function injectStyles (context) {
+  
+  
+}
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  pages_profilevue_type_script_lang_js_,
+  render,
+  staticRenderFns,
+  false,
+  injectStyles,
+  "ad4b7ee2",
+  "10158d98"
+  
+)
+
+/* harmony default export */ var profile = __webpack_exports__["default"] = (component.exports);
+
+/***/ })
+
+};;
+//# sourceMappingURL=6.js.map
